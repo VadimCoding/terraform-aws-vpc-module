b49c8582c174606f862d693f60bdebd56a304cef
Vadim <vt2911@hotmail.com>
2022-08-17 10:09:57 +0200

    Format all files

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "my_vpc" {
-    cidr_block = var.cidr_block
-    enable_dns_support = var.enable_dns_support
-    enable_dns_hostnames = var.enable_dns_hostnames
-    assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
-    tags = {
-        Name = var.name
-    }
+  cidr_block                       = var.cidr_block
+  enable_dns_support               = var.enable_dns_support
+  enable_dns_hostnames             = var.enable_dns_hostnames
+  assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
+  tags = {
+    Name = var.name
+  }
 }
