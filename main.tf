a8210bdd8eecdb1faa0aaada7fe712cbaaf09e23
Vadim <vt2911@hotmail.com>
2022-08-18 11:50:31 +0200

    🛠 Fix typo in resource aws_subnet_private

--- main.tf
+++ main.tf
@@ -25,11 +25,11 @@ resource "aws_subnet" "private" {
   count = length(var.private_subnets)
 
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnets[count.index].cidr_block
-  availability_zone       = var.public_subnets[count.index].availability_zone
+  cidr_block              = var.private_subnets[count.index].cidr_block
+  availability_zone       = var.private_subnets[count.index].availability_zone
   map_public_ip_on_launch = true
 
   tags = {
-    Name = var.public_subnets[count.index].name
+    Name = var.private_subnets[count.index].name
   }
 }
